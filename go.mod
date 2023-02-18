2fbef249d3915aafd6f151f7f86c95d26fd01f57
Johan Jansson <johan.jansson@iki.fi>
2023-02-18 15:43:37 +0200

    Replace module x/crypto/ssh/terminal with x/term
    Per package documentation for x/crypto/ssh/terminal:
    "Deprecated: this package moved to golang.org/x/term."

--- go.mod
+++ go.mod
@@ -2,9 +2,6 @@ module github.com/jupj/qvik
 
 go 1.20
 
-require (
-	golang.org/x/crypto v0.6.0
-	golang.org/x/sys v0.5.0
-)
+require golang.org/x/sys v0.5.0
 
-require golang.org/x/term v0.5.0 // indirect
+require golang.org/x/term v0.5.0
--- raw.go
+++ raw.go
@@ -5,16 +5,16 @@ package main
 import (
 	"os"
 
-	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/term"
 )
 
 // enableRawMode sets the console into raw mode and returns a restore function.
 // If err == nil, restore will restore the console to it's previous mode.
 func enableRawMode() (restore func(), err error) {
 	fd := int(os.Stdin.Fd())
-	oldState, err := terminal.MakeRaw(fd)
+	oldState, err := term.MakeRaw(fd)
 	if err != nil {
 		return nil, err
 	}
-	return func() { terminal.Restore(fd, oldState) }, nil
+	return func() { term.Restore(fd, oldState) }, nil
 }
